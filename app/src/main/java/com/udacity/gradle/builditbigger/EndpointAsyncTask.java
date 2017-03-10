558738196aa00724c002d8ffcde2667cec6484fd
briancap <btc5081@gmail.com>
2017-03-10 13:36:14 -0500

    EndpoitnCleanup

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointAsyncTask.java
@@ -8,6 +8,8 @@ import com.example.brian.displayjoke.JokeActivity;
 import com.example.brian.myapplication.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
@@ -28,7 +30,13 @@ public class EndpointAsyncTask extends AsyncTask<Void, Void, String> {
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/");
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
             // end options for devappserver
 
             myApiService = builder.build();
